a1d5a4b3b1288314fb7779afc2f465c128540b4d
choijungin2 <51218019+choijungin2@users.noreply.github.com>
2019-06-07 19:29:01 +0900

    ㅇㅇ
    ㅇㅇ

--- src/cf/member/model/Joincontrol.java
+++ src/cf/member/model/Joincontrol.java
@@ -2,6 +2,7 @@ package cf.member.model;
  
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -12,99 +13,45 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import cf.member.model.DBConnection;
+import cf.member.model.JoinService;
 
 @WebServlet("/join.do") //얘가 있으면 컨트롤임
 public class Joincontrol extends HttpServlet {
     private static final long serialVersionUID = 1L;
        
     
-    private Connection conn;
-    private Statement stmt;
-    private String query;
-    private String driver,url,uId,uPw,m_id,m_name,m_pwd,m_phone,m_ldate,m_mdate;
-    
-
 	public void service(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		String m = request.getParameter("m");
-		
-		
-		if(m != null) {
-			m = m.trim();
-			switch(m) {
-				case "join" : join(request, response); break;
-				case "logout" : logout(request, response); break;
-				default : join_form(request, response); break; //진심여기어케고쳐야할지 몰겟음
-			}
-		}else {
-			join_form(request, response);
-		}		
-	
+		if(m.equals("join")) {
+			join(request, response);
+			
+		}else {}
 	}
 		
-		
-		
-    public Joincontrol() {
-        super();
-
-    }
 	
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        actionDo(request, response);
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        actionDo(request, response);
-    }
+		
+        private void join(HttpServletRequest request, HttpServletResponse response) 
+    			throws ServletException, IOException {
+       JoinService service = JoinService.getInstance();
+   		String m_id = request.getParameter("m_id");
+	    String m_name = request.getParameter("m_name");
+	    String m_pwd = request.getParameter("m_pwd");
+	    String m_phone = request.getParameter("ph1") + request.getParameter("ph2") + request.getParameter("ph3");
+	 
+	    
+	    MemDTO jdto= new MemDTO(m_id, m_name, m_pwd, m_phone, null ,null);
+       
+       
+       
+       
+       service.joinS(jdto);
+        	
+        	
+        }
+	
+		
+		
+	
  
-    private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        m_id = request.getParameter("m_id");
-        m_name = request.getParameter("m_name");
-        m_pwd = request.getParameter("m_pwd");
-        m_phone = request.getParameter("ph1") + "-" +request.getParameter("ph2") + "-" + request.getParameter("ph3");
-        m_ldate = request.getParameter("m_ldate");
-        m_mdate = request.getParameter("m_mdate");
-        
-        query = "insert into member values('"
-                +m_name+"', '"
-                +m_id+"', '"
-                +m_pwd+"', '"
-                +m_phone+"', '"
-                +m_ldate+"', '"
-                +m_mdate+"')";
- 
-        try{
-            Class.forName(driver);
-            conn = DriverManager.getConnection(url, uId, uPw);
-            stmt = conn.createStatement();
-            int i = stmt.executeUpdate(query);
-            if(i==1){
-                System.out.println("가입 성공");
-                response.sendRedirect("joinok.html");
-            }
-            else{
-                System.out.println("가입 실패");
-                response.sendRedirect("joinfail.html");
-            }
-            
-        }
-        catch(Exception e){
-            e.printStackTrace();
-            System.out.println("가입 실패");
-            response.sendRedirect("Joinfail.html");
-            
-        }
-        finally{
-            try{
-                if(stmt!=null)stmt.close();
-                if(conn!=null)conn.close();
-            }
-            catch(Exception e){
-                e.printStackTrace();
-                
-            }
-        }
-    
-    }
 }
